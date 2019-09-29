3bdb23454cc24a4b702d33700e822f65333f5409
Derek Gregg <42588472+derekjgregg@users.noreply.github.com>
2019-09-29 18:02:41 -0400

    Crane Rotation Update
    Includes addition of crane GameObject, placeholder graphic, and a script to make the GameObject follow the mouse with its rotation.

---
+++ T.R.A.S.H/Assets/Scripts/Crane_Movement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crane_Movement : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 positionOfMouse = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10f);
+        float angleOfRotation = (AngleBetweenCraneAndMouse(transform.position, positionOfMouse)) + 90;
+        transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angleOfRotation));
+    }
+
+    float AngleBetweenCraneAndMouse(Vector2 input1, Vector2 input2)
+    {
+        return Mathf.Rad2Deg * Mathf.Atan2(input1.y - input2.y, input1.x - input2.x);
+    }
+}
