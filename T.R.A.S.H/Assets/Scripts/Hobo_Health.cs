862bae1f8db9ad1a8b92803870a30f98a63de4d1
AaronKettelhut <54749225+AaronKettelhut@users.noreply.github.com>
2019-10-10 16:24:42 -0400

    Hobo health script
    includes basic health attributes and a way to reduce health

---
+++ T.R.A.S.H/Assets/Scripts/Hobo_Health.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hobo_Health : MonoBehaviour
+{
+    public double currentHealth;        // Current accessible health of Hobo      
+    private double totalHealth;         // Hobo Max Health
+    private double healthPercentage;    // Percentage of health 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalHealth = 100.00;           // Set the hobo health to 100
+        currentHealth = totalHealth;    // start the current health to the total health
+        healthPercentage = (currentHealth / totalHealth) * 100; // set healthPercentage
+    }
+    void UpdatePercentage()
+    {
+        healthPercentage = (currentHealth / totalHealth) * 100;
+    }
+    void ReduceHealth()
+    {
+        if (totalHealth <= 0)
+        {
+            return;
+        }
+        // how much the health changes is placeholder atm
+        currentHealth = currentHealth - 0.01;
+        UpdatePercentage();
+    }
+
+    void ReduceHealth(double damageCoefficient)
+    {
+        if (totalHealth <= 0)
+        {
+            return;
+        }
+        // how much the health changes here is also placeholder atm
+        currentHealth = currentHealth - (damageCoefficient * 0.01);
+        UpdatePercentage();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
