c7c97a61c7a172e88a2a098e9a2813ee32a9df33
Derek Gregg <42588472+derekjgregg@users.noreply.github.com>
2019-11-10 14:27:41 -0500

    Rotating Projectile Update
    Includes new script attached to all projectile prefabs, allowing for rotation of projectile either clockwise or counterclockwise during flight, determined by random chance.

--- T.R.A.S.H/Assets/Scripts/Crane_Projectile.cs
+++ T.R.A.S.H/Assets/Scripts/Crane_Projectile.cs
@@ -28,6 +28,8 @@ public class Crane_Projectile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        transform.Rotate(0, 0, 50 * Time.deltaTime);
+
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             projectileCode++;
---
+++ T.R.A.S.H/Assets/Scripts/Projectile_Rotation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile_Rotation : MonoBehaviour
+{
+    int chance = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* Get random value between 0 and 1 */
+        Random.Range(0, 1);
+
+        /* 50% chance to set flag to 1 */
+        if (Random.value < 0.5)
+        {
+            chance = 1;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Rotates projectile clockwise or counterclockwise based on flag */
+        if(chance == 1)
+        {
+            transform.Rotate(0, 0, 100 * Time.deltaTime);
+        } else
+        {
+            transform.Rotate(0, 0, -100 * Time.deltaTime);
+        }
+    }
+}
